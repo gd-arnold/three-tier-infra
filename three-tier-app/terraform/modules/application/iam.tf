35602d45e9a917eef079b9d2cd68e0a08503b65e
gd-arnold <gdarnold19@gmail.com>
2025-02-16 13:23:00 +0200

    tta: feat: ec2 iam role and ssm policy attachement

---
+++ three-tier-app/terraform/modules/application/iam.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_role" "application_role" {
+  name = "${var.name_prefix}-application-iam-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = merge(
+    var.tags,
+    {
+      Name : "${var.name_prefix}-application-iam-role"
+    }
+  )
+}
+
+resource "aws_iam_policy_attachment" "ssm" {
+  name       = "${var.name_prefix}-application-ssm-attachment"
+  roles      = [aws_iam_role.application_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
