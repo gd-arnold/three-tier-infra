2e850838e8f25f7f54c7ea239f20024b2be86c59
gd-arnold <gdarnold19@gmail.com>
2025-02-23 12:11:25 +0200

    tta: fix: db module outputs

--- three-tier-app/terraform/modules/database/outputs.tf
+++ three-tier-app/terraform/modules/database/outputs.tf
@@ -1,3 +1,11 @@
+output "db_instance_arn" {
+  value = aws_db_instance.db.arn
+}
+
+output "db_instance_status" {
+  value = aws_db_instance.db.status
+}
+
 output "db_password_secret_arn" {
   value = aws_secretsmanager_secret.db_password.arn
 }
